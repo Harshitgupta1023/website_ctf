36538bf0ad8b4015cc8bc55ce8c52d2d13301453
gourav2001k <mailatgourav2001@gmail.com>
2021-03-05 01:30:55 +0530

    Minor validation changes

--- server/graphql/validators/problemValidators.js
+++ server/graphql/validators/problemValidators.js
@@ -4,7 +4,7 @@ module.exports = Joi.object({
   title: Joi.string().min(3).required(),
   statement: Joi.string().min(3).required(),
   solution: Joi.string().min(3).required(),
-  points: Joi.number().min(1).max(100).required(),
+  points: Joi.number().min(1).max(1000).required(),
   category: Joi.array().items(Joi.string().min(3)).min(1),
   hints: Joi.array().items(Joi.string().min(3)),
 });
