3cca68b6384c2409a017bd6705d005efeb875033
sarthak750 <sarthakvasan1234@gmail.com>
2021-07-20 19:49:17 +0530

    Adding Info

--- client/src/Components/ToolsCard.js
+++ client/src/Components/ToolsCard.js
@@ -95,7 +95,7 @@ export default function ToolsCard(props) {
                 Description
               </Button>
             </CardActions>
-          </Card>{" "}
+          </Card>
         </div>
       </ReactCardFlip>
     </div>
