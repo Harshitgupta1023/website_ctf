dfbcaa7475a20248d6a14325aa8d6fba1e01da13
Gourav <mailatgourav2001@gmail.com>
2022-12-09 13:23:11 +0530

    Migrate Frontend

--- client/src/layout/Logincard.js
+++ client/src/layout/Logincard.js
@@ -17,321 +17,337 @@ import useForm from "../customHooks/useForm";
 import Loading from "../Components/Loading";
 import { AuthContext } from "../context/auth";
 import { gql, useMutation } from "@apollo/client";
-import { withRouter } from "react-router-dom";
 import GoogleLogin from "react-google-login";
 import { setAccessToken } from "../data/authToken";
+import withRouter from "../utils/withRouter";
 import MessagePopup from "../Components/MessagePopup";
 
 function ConnectWith() {
-  return (
-    <Typography variant="body2" color="primary" align="center">
-      {"or connect with"}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="primary" align="center">
+            {"or connect with"}
+        </Typography>
+    );
 }
 
 function Handles({ history }) {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const [message, setMessage] = React.useState("");
-  const [severity, setSeverity] = React.useState("success");
-  const { login, updateUser } = useContext(AuthContext);
-  const [googleLogin, { loading }] = useMutation(GOOGLE_LOGIN, {
-    onCompleted({ googleLogin: loginData }) {
-      login(loginData);
-      console.log("Yo boi", loginData);
-      history.push("/getstarted");
-    },
-    onError({ graphQLErrors }) {
-      if (graphQLErrors) {
-        graphQLErrors.forEach(({ message }) => {
-          setMessage(message);
-          setSeverity("error");
-          setOpen(true);
-        });
-      }
-    },
-  });
-  const [githubLogin, { gitLoading }] = useMutation(GITHUB_LOGIN, {
-    onCompleted({ githubLogin: { user, token } }) {
-      updateUser(user);
-      setAccessToken(token);
-      history.push("/getstarted");
-    },
-    onError({ graphQLErrors }) {
-      if (graphQLErrors) {
-        graphQLErrors.forEach(({ message }) => {
-          setMessage(message);
-          setSeverity("error");
-          setOpen(true);
-        });
-      }
-    },
-  });
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [message, setMessage] = React.useState("");
+    const [severity, setSeverity] = React.useState("success");
+    const { login, updateUser } = useContext(AuthContext);
+    const [googleLogin, { loading }] = useMutation(GOOGLE_LOGIN, {
+        onCompleted({ googleLogin: loginData }) {
+            login(loginData);
+            console.log("Yo boi", loginData);
+            history.push("/getstarted");
+        },
+        onError({ graphQLErrors }) {
+            if (graphQLErrors) {
+                graphQLErrors.forEach(({ message }) => {
+                    setMessage(message);
+                    setSeverity("error");
+                    setOpen(true);
+                });
+            }
+        },
+    });
+    const [githubLogin, { gitLoading }] = useMutation(GITHUB_LOGIN, {
+        onCompleted({ githubLogin: { user, token } }) {
+            updateUser(user);
+            setAccessToken(token);
+            history.push("/getstarted");
+        },
+        onError({ graphQLErrors }) {
+            if (graphQLErrors) {
+                graphQLErrors.forEach(({ message }) => {
+                    setMessage(message);
+                    setSeverity("error");
+                    setOpen(true);
+                });
+            }
+        },
+    });
 
-  useEffect(() => {
-    const url = window.location.href;
-    const hasCode = url.includes("?code=");
+    useEffect(() => {
+        const url = window.location.href;
+        const hasCode = url.includes("?code=");
 
-    if (hasCode) {
-      const newUrl = url.split("?code=");
-      window.history.pushState({}, null, newUrl[0]);
-      githubLogin({ variables: { code: newUrl[1] } });
-    }
-  });
+        if (hasCode) {
+            const newUrl = url.split("?code=");
+            window.history.pushState({}, null, newUrl[0]);
+            githubLogin({ variables: { code: newUrl[1] } });
+        }
+    });
 
-  const handleGoogleLogin = ({ tokenId }) => {
-    googleLogin({ variables: { id_token: tokenId } });
-  };
-  return loading || gitLoading ? (
-    <Loading loading={loading || gitLoading} />
-  ) : (
-    <div className={classes.handleContainer}>
-      <div className={classes.subHandleContainer}>
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-          buttonText="Login"
-          onSuccess={handleGoogleLogin}
-          onFailure={(res) => console.log("Login Failed!", res)}
-          theme="dark"
-          className={classes.googleLink}
-        />
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}`}
-        >
-          <GitHubIcon className={classes.githubIconStyle} />
-        </a>
-      </div>
-      <MessagePopup
-        open={open}
-        message={message}
-        severity={severity}
-        setOpen={setOpen}
-        loading={loading}
-      />
-    </div>
-  );
+    const handleGoogleLogin = ({ tokenId }) => {
+        googleLogin({ variables: { id_token: tokenId } });
+    };
+    return loading || gitLoading ? (
+        <Loading loading={loading || gitLoading} />
+    ) : (
+        <div className={classes.handleContainer}>
+            <div className={classes.subHandleContainer}>
+                <GoogleLogin
+                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                    buttonText="Login"
+                    onSuccess={handleGoogleLogin}
+                    onFailure={(res) => console.log("Login Failed!", res)}
+                    theme="dark"
+                    className={classes.googleLink}
+                />
+                <a
+                    href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}`}
+                >
+                    <GitHubIcon className={classes.githubIconStyle} />
+                </a>
+            </div>
+            <MessagePopup
+                open={open}
+                message={message}
+                severity={severity}
+                setOpen={setOpen}
+                loading={loading}
+            />
+        </div>
+    );
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    height: "90vh",
-  },
-  image: {
-    backgroundImage: `url(${hackingOAuth})`,
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%",
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    backgroundColor: button.buttonBackgroundColor,
-    color: button.buttonColor,
-    fontWeight: button.buttonFontWeight,
-    fontSize: button.buttonFontSize,
-    textTransform: button.buttonTextTransform,
-    "&:hover": {
-      backgroundColor: button.buttonHoverColor,
+    root: {
+        height: "90vh",
     },
-  },
-  googleLink: {
-    paddingTop: "0",
-  },
-  handleContainer: {
-    display: "flex",
-    justifyContent: "center",
-    marginTop: 20,
-    width: "90%",
-  },
-  subHandleContainer: {
-    width: "50%",
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-  githubIconStyle: {
-    height: "45px",
-    width: "45px",
-    color: "white",
-    paddingTop: "2%",
-  },
+    image: {
+        backgroundImage: `url(${hackingOAuth})`,
+        backgroundColor:
+            theme.palette.type === "light"
+                ? theme.palette.grey[50]
+                : theme.palette.grey[900],
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: "100%",
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+        backgroundColor: button.buttonBackgroundColor,
+        color: button.buttonColor,
+        fontWeight: button.buttonFontWeight,
+        fontSize: button.buttonFontSize,
+        textTransform: button.buttonTextTransform,
+        "&:hover": {
+            backgroundColor: button.buttonHoverColor,
+        },
+    },
+    googleLink: {
+        paddingTop: "0",
+    },
+    handleContainer: {
+        display: "flex",
+        justifyContent: "center",
+        marginTop: 20,
+        width: "90%",
+    },
+    subHandleContainer: {
+        width: "50%",
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+    },
+    githubIconStyle: {
+        height: "45px",
+        width: "45px",
+        color: "white",
+        paddingTop: "2%",
+    },
 }));
 
 const LOGIN_USER = gql`
-  mutation login($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
-      user {
-        id
-        username
-        email
-        imageURL
-        points
-        verified
-        solvedProblems
-      }
-      token
+    mutation login($username: String!, $password: String!) {
+        login(username: $username, password: $password) {
+            user {
+                id
+                username
+                email
+                imageURL
+                points
+                verified
+                solvedProblems
+            }
+            token
+        }
     }
-  }
 `;
 
 const GOOGLE_LOGIN = gql`
-  mutation googleLogin($id_token: String!) {
-    googleLogin(id_token: $id_token) {
-      user {
-        id
-        username
-        email
-        imageURL
-        points
-        verified
-        solvedProblems
-      }
-      token
+    mutation googleLogin($id_token: String!) {
+        googleLogin(id_token: $id_token) {
+            user {
+                id
+                username
+                email
+                imageURL
+                points
+                verified
+                solvedProblems
+            }
+            token
+        }
     }
-  }
 `;
 
 const GITHUB_LOGIN = gql`
-  mutation githubLogin($code: String!) {
-    githubLogin(code: $code) {
-      user {
-        id
-        username
-        email
-        imageURL
-        points
-        verified
-        solvedProblems
-      }
-      token
+    mutation githubLogin($code: String!) {
+        githubLogin(code: $code) {
+            user {
+                id
+                username
+                email
+                imageURL
+                points
+                verified
+                solvedProblems
+            }
+            token
+        }
     }
-  }
 `;
 
 function SignInSide(props) {
-  const classes = useStyles();
-  const context = useContext(AuthContext);
-  const [open, setOpen] = React.useState(false);
-  const [message, setMessage] = React.useState("");
-  const [severity, setSeverity] = React.useState("success");
-  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
-    update(_, { data: { login: userData } }) {
-      context.login(userData);
-      props.history.push("/getstarted");
-    },
-    onError({ graphQLErrors }) {
-      if (graphQLErrors) {
-        graphQLErrors.forEach(({ message }) => {
-          setMessage(message);
-          setSeverity("error");
-          setOpen(true);
-        });
-      }
-    },
-  });
-  const { formInputs, handleInputChange, handleSubmit } = useForm(
-    { username: "", password: "" },
-    () => {
-      loginUser({ variables: formInputs });
-    }
-  );
-  if (loading) <Loading />;
-  return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Log in
-          </Typography>
-          <form className={classes.form} onSubmit={handleSubmit}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              onChange={handleInputChange}
-              label="Username"
-              name="username"
-              value={formInputs.username}
-              autoComplete="username"
-              autoFocus
-            />
+    const classes = useStyles();
+    const context = useContext(AuthContext);
+    const [open, setOpen] = React.useState(false);
+    const [message, setMessage] = React.useState("");
+    const [severity, setSeverity] = React.useState("success");
+    const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+        update(_, { data: { login: userData } }) {
+            context.login(userData);
+            props.history.push("/getstarted");
+        },
+        onError({ graphQLErrors }) {
+            if (graphQLErrors) {
+                graphQLErrors.forEach(({ message }) => {
+                    setMessage(message);
+                    setSeverity("error");
+                    setOpen(true);
+                });
+            }
+        },
+    });
+    const { formInputs, handleInputChange, handleSubmit } = useForm(
+        { username: "", password: "" },
+        () => {
+            loginUser({ variables: formInputs });
+        }
+    );
+    if (loading) <Loading />;
+    return (
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid
+                item
+                xs={12}
+                sm={8}
+                md={5}
+                component={Paper}
+                elevation={6}
+                square
+            >
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Log in
+                    </Typography>
+                    <form className={classes.form} onSubmit={handleSubmit}>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            onChange={handleInputChange}
+                            label="Username"
+                            name="username"
+                            value={formInputs.username}
+                            autoComplete="username"
+                            autoFocus
+                        />
 
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              value={formInputs.password}
-              onChange={handleInputChange}
-              autoComplete="current-password"
-            />
-            {/* <FormControlLabel
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            value={formInputs.password}
+                            onChange={handleInputChange}
+                            autoComplete="current-password"
+                        />
+                        {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             /> */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Log in
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link to="forgotPass" variant="body2" className="links">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link to="signup" variant="body2" className="links">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Log in
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link
+                                    to="forgotPass"
+                                    variant="body2"
+                                    className="links"
+                                >
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link
+                                    to="signup"
+                                    variant="body2"
+                                    className="links"
+                                >
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                        <Box mt={5}>
+                            <ConnectWith />
+                            <Handles history={props.history} />
+                        </Box>
+                    </form>
+                </div>
             </Grid>
-            <Box mt={5}>
-              <ConnectWith />
-              <Handles history={props.history} />
-            </Box>
-          </form>
-        </div>
-      </Grid>
-      <MessagePopup
-        open={open}
-        message={message}
-        severity={severity}
-        setOpen={setOpen}
-        loading={loading}
-      />
-    </Grid>
-  );
+            <MessagePopup
+                open={open}
+                message={message}
+                severity={severity}
+                setOpen={setOpen}
+                loading={loading}
+            />
+        </Grid>
+    );
 }
 
 export default withRouter(SignInSide);
--- client/src/layout/ProblemsSection.js
+++ client/src/layout/ProblemsSection.js
@@ -1,82 +1,86 @@
 import React from "react";
 import QuestionCard from "../Displayquestion/QuestionCard";
 import { makeStyles } from "@material-ui/core/styles";
-import { withRouter } from "react-router-dom";
 import Showquestion from "../testing/Showquestion";
 import { Dialog } from "@material-ui/core";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth";
+import withRouter from "../utils/withRouter";
 
 const useStyles = makeStyles(() => ({
-  topppp: {
-    transition: "all 0.2s ease-in-out",
-    opacity: 0.7,
-    "&:hover": {
-      transform: "scale3d(1.1, 1.1, 1.1)",
-      opacity: 1,
+    topppp: {
+        transition: "all 0.2s ease-in-out",
+        opacity: 0.7,
+        "&:hover": {
+            transform: "scale3d(1.1, 1.1, 1.1)",
+            opacity: 1,
+        },
     },
-  },
 }));
 
 function ProblemsSection(props) {
-  // console.log(props);
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const [ids, setIds] = React.useState("");
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-    props.history.push({ pathname: `${props.location.pathname}` });
-  };
-  const { user } = useContext(AuthContext);
+    // console.log(props);
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [ids, setIds] = React.useState("");
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+        props.history.push({ pathname: `${props.location.pathname}` });
+    };
+    const { user } = useContext(AuthContext);
 
-  return (
-    <div className="topics-section">
-      {props.questions.map((question, index) => (
-        <div
-          className={classes.topppp}
-          key={question.id}
-          onClick={(e) => {
-            setIds(question.id);
-            handleClickOpen();
-            props.history.push({
-              pathname: `${props.location.pathname}`,
-              search:
-                "?" +
-                new URLSearchParams({ questionId: question.id }).toString(),
-            });
-          }}
-        >
-          <QuestionCard
-            cssapply
-            onClick={() => handleClickOpen}
-            width={350}
-            solved={
-              user.solvedProblems.indexOf(question.id) === -1 ? false : true
-            }
-            height={250}
-            category={question.category}
-            className="question"
-            points={question.points}
-            title={question.title}
-            statement={question.statement}
-            fileURL={question.fileURL}
-            hints={question.hints}
-          />
-          {/* </Link> */}
+    return (
+        <div className="topics-section">
+            {props.questions.map((question, index) => (
+                <div
+                    className={classes.topppp}
+                    key={question.id}
+                    onClick={(e) => {
+                        setIds(question.id);
+                        handleClickOpen();
+                        props.history.push({
+                            pathname: `${props.location.pathname}`,
+                            search:
+                                "?" +
+                                new URLSearchParams({
+                                    questionId: question.id,
+                                }).toString(),
+                        });
+                    }}
+                >
+                    <QuestionCard
+                        cssapply
+                        onClick={() => handleClickOpen}
+                        width={350}
+                        solved={
+                            user.solvedProblems.indexOf(question.id) === -1
+                                ? false
+                                : true
+                        }
+                        height={250}
+                        category={question.category}
+                        className="question"
+                        points={question.points}
+                        title={question.title}
+                        statement={question.statement}
+                        fileURL={question.fileURL}
+                        hints={question.hints}
+                    />
+                    {/* </Link> */}
+                </div>
+            ))}
+            <Dialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+            >
+                <Showquestion location={props.location.pathname} id={ids} />
+            </Dialog>
         </div>
-      ))}
-      <Dialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <Showquestion location={props.location.pathname} id={ids} />
-      </Dialog>
-    </div>
-  );
+    );
 }
 
 export default withRouter(ProblemsSection);
